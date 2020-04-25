5d52029cd241e00318399755a3f3ca141f860664
Mark Timothy Vasconcelos Meehan <mtimothy38@gmail.com>
2020-04-25 15:25:48 +0100

    Create CodeGenerator class and migrate respective functions

---
+++ src/CodeGenerator.java
@@ -0,0 +1,97 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.SimpleTimeZone;
+
+public class CodeGenerator {
+    static String generated;
+    private SimpleNode root;
+    
+
+    public CodeGenerator(SimpleNode root) {
+        this.root = root;
+        generated = "";
+    }
+
+
+    public void generate() {
+        //(verificar se o root é classe, ver os children e por aí fora)
+        Node[] children =  root.jjtGetChildren();
+
+        for(Node child: children) {
+            if (child.getId() == 6) {
+                Node classChilds[] = displayClass(child);
+                displayVariables(classChilds);
+                displayMainMethod(classChilds);
+            }
+        }
+        print();        
+    }
+
+
+    static Node[] displayClass(Node node) {
+        SimpleNode classSimpleNode = (SimpleNode) node;
+        Node[] classChilds = classSimpleNode.jjtGetChildren();
+
+        generated += ".public class " + ((ASTIDENT) classChilds[0]).name + '\n';
+        generated += ".super java/lang/Object\n";
+
+        return classChilds;
+    }
+
+
+    static void displayVariables(Node node[]) {
+        for (Node n : node) {
+            SimpleNode simpleN = (SimpleNode) n;
+
+            if (simpleN.toString().equals("VARIABLE")) {
+                generated += ".field " + ((ASTIDENT) simpleN.jjtGetChildren()[1]).name + " ";
+
+                ASTTYPE typeN = (ASTTYPE)simpleN.jjtGetChildren()[0];
+                switch (typeN.typeID) {
+                    case STRING:
+                        generated += "V\n";
+                        break;
+                    case INT:
+                        generated += "I\n";
+                        break;
+                    case BOOL:
+                        generated += "Z\n";
+                        break;
+                }
+            }
+        }
+    }
+
+
+    static void displayMainMethod(Node node[]) {
+        for (Node n : node){
+            SimpleNode simpleN = (SimpleNode) n;
+            if (simpleN.toString().equals("MAINMETHOD")) {
+                //Do stuff
+            }
+        }
+    }
+
+
+    public PrintWriter fileToWrite() throws IOException {
+
+        File dir = new File("jvm");
+        if (!dir.exists()) dir.mkdirs();
+
+        File file = new File("jvm/jasmin.j");
+
+        if(!file.exists())
+            file.createNewFile();
+
+        PrintWriter writer = new PrintWriter(file);
+        return writer;
+    }
+
+
+    public void print() {
+        System.out.println(this.generated);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.util.SimpleTimeZone;
 public class Main {
     
     static String tmp = "";
+    static CodeGenerator codeGenerator;
 
     public static void main(String[] args) throws ParseException {
 
@@ -29,98 +30,17 @@ public class Main {
                 root.eval();
                 System.out.println("\n"+root.getId()+ "\n");
                 root.dump(""); // prints the tree on the screen
-                printJasmin(root);
+                codeGenerator = new CodeGenerator(root);
+                codeGenerator.generate();
             } catch (Exception e){
                 e.printStackTrace();
             }
 
             //root.dump(""); // prints the tree on the screen
 
-            ////
-
-            //root.printJasmin();
-
-            ///
-
         } catch (ParseException e) {
             e.printStackTrace();
             throw new ParseException("Parser error");
-        }
-    }
-
-    public PrintWriter fileToWrite() throws IOException {
-
-        File dir = new File("jvm");
-        if (!dir.exists()) dir.mkdirs();
-
-        File file = new File("jvm/jasmin.j");
-
-        if(!file.exists())
-            file.createNewFile();
-
-        PrintWriter writer = new PrintWriter(file);
-        return writer;
-
-    }
-
-    static void printJasmin(SimpleNode root) throws IOException {
-
-        //PrintWriter file = fileToWrite();
-
-
-        //(verificar se o root é classe, ver os children e por aí fora)
-        Node[] children =  root.jjtGetChildren();
-
-        for(int i = 0; i < children.length; i++) {
-            if (children[i].getId() == 6) {
-                Node classChilds[] = displayClass(children[i]);
-                displayVariables(classChilds);
-                displayMainMethod(classChilds);
-            }
-        }
-
-        System.out.println(tmp);
-    }
-
-    static Node[] displayClass(Node node) {
-        SimpleNode classSimpleNode = (SimpleNode) node;
-        Node[] classChilds = classSimpleNode.jjtGetChildren();
-
-        tmp += ".public class " + ((ASTIDENT) classChilds[0]).name + '\n';
-        tmp += ".super java/lang/Object\n";
-
-        return classChilds;
-    }
-
-    static void displayVariables(Node node[]) {
-        for (Node n : node) {
-            SimpleNode simpleN = (SimpleNode) n;
-            if (simpleN.toString().equals("VARIABLE")) {
-
-                tmp += ".field " + ((ASTIDENT) simpleN.jjtGetChildren()[1]).name + " ";
-
-                ASTTYPE typeN = (ASTTYPE)simpleN.jjtGetChildren()[0];
-                switch (typeN.typeID) {
-                    case STRING:
-                        tmp += "V\n";
-                        break;
-                    case INT:
-                        tmp += "I\n";
-                        break;
-                    case BOOL:
-                        tmp += "Z\n";
-                        break;
-                }
-            }
-        }
-    }
-
-    static void displayMainMethod(Node node[]) {
-        for (Node n : node){
-            SimpleNode simpleN = (SimpleNode) n;
-            if (simpleN.toString().equals("MAINMETHOD")) {
-
-            }
         }
     }
 
